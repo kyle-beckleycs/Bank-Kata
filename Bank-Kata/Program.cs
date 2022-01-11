dba0add95203d0685262c6ad458ef4543c9ffb70
Kyle Beckley <748901@corp.trizetto.com>
2022-01-11 14:07:40 -0600

    Add project files.

---
+++ Bank-Kata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bank_Kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
